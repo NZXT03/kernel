e99bef395755aca14ad6a8f9ceb835daac86c9f8
NZXT03 <66913234+NZXT03@users.noreply.github.com>
2020-06-26 22:28:30 +0600

    1

--- unified.sh
+++ unified.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/android_kernel_xiaomi_santoni-4.9
+cd root/project/android_kernel_xiaomi_santoni-4.9
 
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 gcc-64 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 gcc-32 --depth=1
