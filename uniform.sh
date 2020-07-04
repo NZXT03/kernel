659b9096424b4f4d16ecf4e40b25049488fbbd0a
NZXT03 <66913234+NZXT03@users.noreply.github.com>
2020-07-04 12:54:42 +0600

    2

--- uniform.sh
+++ uniform.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-mkdir /root/project/android-kernel-xiaomi-santoni-4.9/clang
-wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/b8b61fff8660c2f7262939b7cb9dde7de38df133/clang-r365631c2.tar.gz
-tar xzvf clang-r365631c2.tar.gz -C /root/project/android-kernel-xiaomi-santoni-4.9/clang
 
-apt update -y && sudo apt upgrade -y
+apt update -y && apt upgrade -y
 apt-get install --no-install-recommends -y \
  autoconf \
  autogen \
