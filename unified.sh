4d8ef877f4d5c62cfa8f64e18e407fa1db31736c
NZXT03 <66913234+NZXT03@users.noreply.github.com>
2020-07-04 11:33:26 +0600

    Update unified.sh

--- unified.sh
+++ unified.sh
@@ -4,13 +4,22 @@ cd /root/project/android_kernel_xiaomi_santoni-4.9
 
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-17.1 gcc-64 --depth=1
 git clone https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-17.1 gcc-32 --depth=1
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 clang
 
 rm -rf out
 
 make O=out clean && make O=out mrproper
 make O=out ARCH=arm64 santoni_treble_defconfig
 
+PATH="$(pwd)/clang/bin:$(pwd)/gcc-64/bin:$(pwd)/gcc-32/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
-                      CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="$(pwd)/gcc-32/bin/arm-linux-androideabi-"
+                      CC=clang \
+                      CLANG_TRIPLE=aarch64-linux-gnu- \
+                      CROSS_COMPILE=aarch64-linux-android- \
+                      CROSS_COMPILE_ARM32=arm-linux-androideabi-
+
+#make -j$(nproc --all) O=out \
+                      #ARCH=arm64 \
+                      #CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-linux-android-" \
+                      #CROSS_COMPILE_ARM32="$(pwd)/gcc-32/bin/arm-linux-androideabi-"
